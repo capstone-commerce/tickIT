40389d114383a648c84257815b7226a99cf4ec0b
root <root@capstone3.cs.kent.edu>
2019-03-21 16:31:06 -0400

    adding fix to spoofs for old authenticate.php

--- src/authenticate.php
+++ src/authenticate.php
@@ -10,17 +10,17 @@
 	//spoof login info below until DB is implemented
 	$spoofAdminUsername = "admin";
 	$spoofAdminPassword = "password";
-	$spoofAdminUsertype = "admin";
+//	$spoofAdminUsertype = "admin";
 
 	$spoofTechUsername = "tech";
 	$spoofTechPassword = "password";
-	$spoofTechUsertype = "tech";
+//	$spoofTechUsertype = "tech";
 
 	if((($_SESSION["username"] == $spoofAdminUsername) and ($_SESSION["password"] == $spoofAdminPassword)) or 
 	(($_SESSION["username"] == $spoofTechUsername)) and ($_SESSION["password"] == $spoofTechPassword)){
 		switch($_SESSION["username"]){
-			case "admin":	$_SESSION["user_type"] = $spoofAdminUsertype; break;
-			case "tech":	$_SESSION["user_type"] = $spoofTechUsertype; break;
+			case "admin":	$_SESSION["user_type"] = "Administrator"; break;
+			case "tech":	$_SESSION["user_type"] = "Technician"; break;
 		}
 		header("Location: ./home.php");
 		exit();	//good practice to exit() after redirect, to ensure below code does not execute
